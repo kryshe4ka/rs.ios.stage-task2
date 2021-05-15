223b2b72072083602244fd499a752e6054837be2
kryshe4ka <kryshe4ka@gmail.com>
2021-05-15 13:18:21 +0300

    feat: add implementation for ReverseBinary task

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,40 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    
+
+    UInt8 reverseInt = 0;
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+    
+    if (n == 0) {
+        return reverseInt; // reverseInt = 0
+    }
+    //    из числа в бинарный массив в обратном порядке
+    while (!(n <= 1)) {
+        [array addObject:@(n % 2)];
+        n = n / 2;
+    }
+    [array addObject:@(1)];
+    // добавить недостающие нули
+        
+    if (array.count < 8) {
+        NSUInteger lenght = 8 - array.count;
+        for (int i = 0; i < lenght; i++) {
+            [array addObject:@(0)];
+        }
+    }
+    
+    //    из массива в новое число
+    for (int i = 0; i < array.count; i++) {
+        reverseInt += [array[i] integerValue] * pow(2, array.count-1-i);
+    }
+        
+    //    вернуть полученное число
+    return reverseInt;
 }
+
+// число будем делить на два, и записывать числа в строку или в массив чисел
+// 22 -> 10110
+// 01101000 -> в число в десятичной
+// 0 * 2 в степени (длина массива- 1)
+// 1 * 2 в степени (длина массива - 2) и т.д.
