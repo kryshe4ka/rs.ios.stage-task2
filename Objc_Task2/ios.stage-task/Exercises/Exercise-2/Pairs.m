04b072aed8d49010b150fd55546d3be7a43c8931
kryshe4ka <kryshe4ka@gmail.com>
2021-05-15 14:22:29 +0300

    feat: add implementation for Pairs task

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,18 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    
+    NSInteger count = 0;
+    NSInteger lenght = array.count;
+    for (int i = 0; i < lenght; i++) {
+        for (int j = i; j < lenght; j++) {
+            if (abs([array[i] intValue] - [array[j] intValue]) == [number intValue]) {
+                count++;
+            }
+        }
+    }
+    
+    return count;
 }
 
 @end
