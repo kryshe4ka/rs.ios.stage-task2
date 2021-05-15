229c314e82f40832ba9cb32dc9ba4572143dec8f
kryshe4ka <kryshe4ka@gmail.com>
2021-05-15 22:45:51 +0300

    fix: fix Pairs task

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -7,7 +7,7 @@
     NSInteger count = 0;
     NSInteger lenght = array.count;
     for (int i = 0; i < lenght; i++) {
-        for (int j = i; j < lenght; j++) {
+        for (int j = i+1; j < lenght; j++) {
             if (abs([array[i] intValue] - [array[j] intValue]) == [number intValue]) {
                 count++;
             }
