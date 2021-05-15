abb775782e238e1ad63a79b3adf960c648f8d0ba
kryshe4ka <kryshe4ka@gmail.com>
2021-05-15 21:51:59 +0300

    feat: add TwoDimensionalArraySorter implementation

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,45 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    
+    if (array == nil || array.count == 0) {
+        return @[];
+    }
+    
+    for (int i = 0; i < array.count; i++) {
+        if (!([array[i] isKindOfClass:[NSArray class]])) {
+            return @[];
+        }
+    }
+    
+    NSMutableArray *numberArray = [NSMutableArray arrayWithArray:@[]];
+    NSMutableArray *stringArray = [NSMutableArray arrayWithArray:@[]];
+    
+    for (int j = 0; j < [array count]; j++) {
+        for (int i = 0; i < [array[j] count]; i++) {
+            if ([array[j][i] isKindOfClass:[NSNumber class]]) {
+                [numberArray addObject:array[j][i]];
+            } else {
+                [stringArray addObject:array[j][i]];
+            }
+        }
+    }
+    
+    NSArray *numberArraySorted = [numberArray sortedArrayUsingSelector:@selector(compare:)];
+    NSArray *stringArraySorted = [stringArray sortedArrayUsingSelector:@selector(compare:)];
+        
+    if (stringArray.count == 0) {
+        return numberArraySorted;
+    } else if (numberArray.count == 0) {
+        return stringArraySorted;
+    } else {
+        
+        NSArray* reversedArray = [[stringArraySorted reverseObjectEnumerator] allObjects];
+
+        return @[numberArraySorted, reversedArray];
+    }
 }
 
 @end
+
+
